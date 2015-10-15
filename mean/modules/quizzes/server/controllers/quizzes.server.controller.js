af6176b15d21d0076545e2807c4cce10a4fff289
botello <betodelrio@gmail.com>
2015-10-15 00:50:17 -0500

    Added Quiz item based on Article item

---
+++ mean/modules/quizzes/server/controllers/quizzes.server.controller.js
@@ -0,0 +1,115 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var path = require('path'),
+  mongoose = require('mongoose'),
+  Quiz = mongoose.model('Quiz'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+
+/**
+ * Create a quiz
+ */
+exports.create = function (req, res) {
+  var quiz = new Quiz(req.body);
+
+  var now = new Date();
+  quiz.created_at = now;
+  quiz.updated_at = now;
+  quiz.user = req.user;
+
+  quiz.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(quiz);
+    }
+  });
+};
+
+/**
+ * Show the current quiz
+ */
+exports.read = function (req, res) {
+  res.json(req.quiz);
+};
+
+/**
+ * Update a quiz
+ */
+exports.update = function (req, res) {
+  var quiz = req.quiz;
+
+  quiz.updated_at = new Date();
+  quiz.title = req.body.title;
+  quiz.content = req.body.content;
+
+  quiz.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(quiz);
+    }
+  });
+};
+
+/**
+ * Delete an quiz
+ */
+exports.delete = function (req, res) {
+  var quiz = req.quiz;
+
+  quiz.remove(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(quiz);
+    }
+  });
+};
+
+/**
+ * List of Quizzes
+ */
+exports.list = function (req, res) {
+  Quiz.find().sort('-created_at').populate('user', 'displayName').exec(function (err, quizzes) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(quizzes);
+    }
+  });
+};
+
+/**
+ * Quiz middleware
+ */
+exports.quizByID = function (req, res, next, id) {
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Quiz is invalid'
+    });
+  }
+
+  Quiz.findById(id).populate('user', 'displayName').exec(function (err, quiz) {
+    if (err) {
+      return next(err);
+    } else if (!quiz) {
+      return res.status(404).send({
+        message: 'No quiz with that identifier has been found'
+      });
+    }
+    req.quiz = quiz;
+    next();
+  });
+};
---
+++ mean/modules/quizzes/server/models/quiz.server.model.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Quiz Schema
+ */
+var QuizSchema = new Schema({
+  created_at: {
+    type: Date
+  },
+  updated_at: {
+    type: Date
+  },
+  title: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Title cannot be blank'
+  },
+  content: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+mongoose.model('Quiz', QuizSchema);
